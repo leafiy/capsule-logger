f0065ca40bc763a3badebb893a42125d0d85d4d0
leafiy <t@leafiy.com>
2019-05-21 15:05:38 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const warnFormat =
+  '[{{title}}] {{timestamp}} {{message}} (in {{path}} {{file}}:{{line}})'
+const errorFormat =
+  '[{{title}}] {{timestamp}} {{message}} (in {{path}} {{file}}:{{line}})\nCall Stack:\n{{stack}}'
+const conf = {
+  format: [
+    '[{{title}}]{{timestamp}}\t{{message}}\t(in {{file}}:{{line}})',
+
+    {
+      log: '{{timestamp}}\t{{message}}\t(in {{file}}:{{line}})',
+      warn: warnFormat,
+      error: errorFormat,
+      fatal: errorFormat
+    }
+  ],
+  dateformat: 'HH:MM:ss.L'
+}
+const logger = (function(logPath = '/logs', maxFiles = 30, level = 'warn') {
+  return process.env.NODE_ENV == 'development' ? require('tracer').colorConsole(conf) : require('tracer').dailyfile({
+    root: path.join(__dirname, logPath),
+    maxLogFiles: maxFiles,
+    format: [
+      '[{{title}}]{{timestamp}} {{message}} (in {{file}}:{{line}})',
+      {
+        warn: warnFormat,
+        error: errorFormat,
+        fatal: errorFormat
+      }
+    ],
+    dateformat: 'yyyy-mm-dd HH:MM:ss.L o',
+    level: level
+  })
+
+
+
+})()
+
+
+module.exports = logger
